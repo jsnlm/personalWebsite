9b78d38ffd5b815491a8689498a6cb33300154c2
jsnlm <jasonjslam@gmail.com>
2016-01-21 02:11:59 -0500

    Just putting what I already have in source control

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    console.log("hello");
+    // $( window ).resize();
+  });
+
+// $( window ).resize(function() {
+//   //console.log(window.innerWidth);
+
+//   //console.log("aaaa = " + Math.floor(window.innerWidth/$(".project").outerWidth(true))  );
+//   if (Math.floor(window.innerWidth/$(".project").outerWidth(true)) == 0){
+//   	$(".body").width( $(".project").outerWidth(true) );
+//   }
+//   else{
+//   	$(".body").width( Math.floor(window.innerWidth/$(".project").outerWidth(true)) * $(".project").outerWidth(true) );	
+//   }
+  
+// });
