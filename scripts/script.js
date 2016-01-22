c2a4a2ae6804d78600686388d71edf706a79a826
jsnlm <jasonjslam@gmail.com>
2016-01-21 21:00:26 -0500

    Finished the content of the site, Still Requires Styling though

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,35 @@
 $( document ).ready(function() {
-    console.log("hello");
-    // $( window ).resize();
+  console.log("hello");
+  
+  $(".header .items").hover(
+		function(){ $(this).addClass("hover"); },
+		function(){ $(this).removeClass("hover"); }
+	);
+
+
+  $(".items.projects").click(function(){
+		$(window).scrollTo( $("#projects"), 500);
   });
+  $(".items.experience").click(function(){
+  	$(window).scrollTo( $("#Experience"), 500);
+  });
+  $(".items.contactMe").click(function(){
+  	$(window).scrollTo( $("#ContactMe"), 500);
+  });
+
+
+});
+
+// $(".header .items").hover(
+// 	function(){
+// 		console.log("a");
+// 		$(this).addClass("hover");
+// 	},
+// 	function(){
+// 		console.log("b");
+// 		$(this).removeClass("hover");
+// 	}
+// );
 
 // $( window ).resize(function() {
 //   //console.log(window.innerWidth);
